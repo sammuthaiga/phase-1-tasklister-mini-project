6a9e827f7713c1265ae9452e1bd03dcf1403eed6
Samuel <samuel.muthaiga@student.moringaschool.com>
2022-12-05 10:14:30 +0300

    Completed the task-lister-mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e)=>{
+      e.preventDefault()
+      handleToDo(e.target.newTaskDescription.value)
+      form.reset()
+  } )
 });
+
+function handleToDo(todo){
+ 
+  
+ let li = document.createElement("li")
+ //button
+ let btn = document.createElement("button") 
+ //Listening to the event when the delete button is clicked
+ btn.addEventListener("click", handleDelete)
+ //Assigning values delete button
+ btn.textContent= "x"
+ // Assigning text to the element
+ li.textContent = `${todo}`
+ // 
+ li.appendChild(btn)
+ //document.querySelector("#tasks").appendChild(li)
+ document.getElementById("tasks").appendChild(li)
+}
+//creating a function that deletes done todo list items
+ function handleDelete(e){
+  e.target.parentNode.remove()
+ }
